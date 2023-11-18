7695f12584754e1d72066a466942715bb09c12c0
aluno <aluno@B25-10>
2023-11-18 16:43:51 -0300

    Adicionar arquivos de projeto.

---
+++ Listas/Funcionarios.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    internal class Funcionario : Conexao
+    {
+        public int id;
+
+        public int id_empresa;
+
+        public string nome;
+
+        public string email;
+
+        private string senha;
+
+        public string nivel;
+
+        public DateTime data_cadastro;
+
+        public void SetSenha(string senha)
+        {
+            this.senha = senha;
+        }
+
+        public string GetSenha()
+        {
+            return this.senha;
+        }
+
+        public List<Funcionario> GetListaFuncionarios()
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            try
+            {
+
+                OpenConnection();
+
+                string query = "SELECT * FROM funcionarios";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            Funcionario novoFuncionario = new Funcionario();
+
+                            novoFuncionario.id = Convert.ToInt32(reader.GetString("id"));
+                            novoFuncionario.id_empresa = Convert.ToInt32(reader.GetString("id_empresa"));
+                            novoFuncionario.nome = reader.GetString("nome");
+                            novoFuncionario.email = reader.GetString("email");
+                            novoFuncionario.nivel = reader.GetString("nivel");
+                            novoFuncionario.data_cadastro = DateTime.Parse(reader.GetString("data_cadastro"));
+
+                            novoFuncionario.SetSenha(reader.GetString("senha"));
+
+                            funcionarios.Add(novoFuncionario);
+                        }
+
+                    }
+
+                }
+
+                CloseConnection();
+
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+
+            return funcionarios;
+        }
+    }
+}
---
+++ Listas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente clientes = new Cliente();
+
+            foreach (Cliente cliente in clientes.GetListaClientes())
+            {
+                Console.WriteLine("ID: " + cliente.id);
+                Console.WriteLine("ID_EMPRESA: " + cliente.id_empresa);
+                Console.WriteLine("NOME: " + cliente.nome);
+                Console.WriteLine("TELEFONE: " + cliente.telefone);
+                Console.WriteLine("EMAIL: " + cliente.email);
+                Console.WriteLine("DOCUMENTO: " + cliente.GetDocumento());
+                Console.WriteLine("DATA_CADASTRO: " + cliente.data_cadastro);
+                Console.WriteLine("-------------------");
+            }
+            Console.ReadLine();
+
+
+            Funcionario funcionarios = new Funcionario();
+
+            foreach (Funcionario funcionario in funcionarios.GetListaFuncionarios())
+            {
+                Console.WriteLine("ID: " + funcionario.id);
+                Console.WriteLine("ID_EMPRESA: " + funcionario.id_empresa);
+                Console.WriteLine("NOME: " + funcionario.nome);
+                Console.WriteLine("EMAIL: " + funcionario.email);
+                Console.WriteLine("NIVEL: " + funcionario.nivel);
+                Console.WriteLine("SENHA: " + funcionario.GetSenha());
+                Console.WriteLine("DATA_CADASTRO: " + funcionario.data_cadastro);
+                Console.WriteLine("-------------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
